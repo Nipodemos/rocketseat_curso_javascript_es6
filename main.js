31eeacd0a6e30be0ebdbfb68eddccd22fbabc3e1
Nipodemos <nipodemos13@gmail.com>
2019-07-24 01:39:01 -0300

    Classes
    Commit que tem introdução de classes e como escreve-las
    Mostra também como instanciar elas e usar seus métodos
    
    Assim como ensinou também o conceito de herança, em que
    uma classe pode usar métodos de outras classes junto com a própria
    
    e também aprendi sobre o conceito de métodos abstratos, que não precisa
    inicializar a variável da classe para usar

--- main.js
+++ main.js
@@ -1,10 +1,75 @@
-alert('Teste');
+/* Começo do ensinamento*/
 
-class Test {
-    metodo() {
-        var i = 1;
+class List {
+    constructor(){
+        this.data = [];
     }
-    outroMetodo() {
-        let j = 2;
+    
+    add(data){
+        this.data.push(data)
     }
 }
+
+class TodoList  extends List {
+    constructor(){
+        super();
+        this.usuario = 'Alan';
+    }
+    
+    mostraUsuario(){
+        console.log(this.usuario);
+    }
+}
+
+const minhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function () {
+    minhaLista.add('Novo Todo');
+}
+
+
+
+/*Método estático
+Basicamente é algum método de uma classe que vc não precisa instanciar pra poder acessar.
+Normalmente vc faz:
+
+var minhaClasse = Classe();
+minhaClasse.metodo();
+
+mas com métodos estáticos, vc pode invocar direto, sem inicializar variável:
+
+Classe.metodo();
+
+*/
+class TodoListEstatica {
+    constructor(){
+        this.todos = [];
+        
+    }
+    /**
+     * Lembrete: métodos estáticos não sabem de nada do resto da classe
+     * então por exmeplo, esse addTodo na verdade não sabe da existencia do array todos
+     * então esse código gera um erro e não funciona
+     */
+    static addTodo(){
+        this.todos.push('Novo Todo');
+        console.log
+    }
+}
+
+TodoList.addTodo();
+TodoList.addTodo();
+TodoList.addTodo();
+TodoList.addTodo();
+
+/**
+ * Exemplo de método abstrato funcional
+*/
+
+class Matematica {
+    static soma(a, b) {
+        return a + b;
+    }
+}
+
+console.log(Matematica.soma(1,3));
