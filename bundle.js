a023ea311d050e51b308b7ca1913f143b037c291
Nipodemos <nipodemos13@gmail.com>
2019-07-25 00:47:55 -0300

    Arrow Functions
    uma forma de simplificar uma função anonima
    
    primeira forma: retire a palavra function e coloque um sinal de igual e maior antes do abre-chaves
    
    segunda forma: se o método tem só um parâmetro, vc pode tirar os parenteses antes da seta
    
    terceira forma: se o conteúdo do método é só uma linha,
    e essa linha for pra retornar algum valor,
    tire o abre-chave e fecha-chave e o "return"
    Observação do Nipodemos: pra te falar a verdade, acho que ficou limpo até demais a sintaxe,
    fica até meio dificil perceber de cara que é uma função anonima com parametro item que retorna item * 2
    
    detalhe:
    vc pode retornar normalmente strings, numeros, arrays e talz, mas quando vc for retornar um objeto, tem uma
    sintaxe diferente
    isso é porque quando vc coloca o abre-chaves ali, ele já acha que é um método que tá seguindo a segunda forma
    que eu citei umas linhas acima

--- bundle.js
+++ bundle.js
@@ -2,39 +2,55 @@
 
 var arr = [1, 2, 3, 4, 5, 7, 10, 13];
 /**
- * map:
- * ele entra em cada um dos items no array e  executa uma function sobre ele.
- * No final é retornado uma nova array com as  * modificações feitas
+ * primeira forma: retire a palavra function e coloque um sinal de igual e maior antes do abre-chaves 
  */
 
-var novoArray = arr.map(function (item, index) {
+var novoArray = arr.map(function (item) {
   return item * 2;
 });
 console.log(novoArray);
 /**
- * reduce:
- * basicamente é uma função para juntar todos os valores dentro de uma array de números
+ * segunda forma: se o método tem só um parâmetro, vc pode tirar os parenteses antes da seta
  */
 
-var sum = arr.reduce(function (anterior, atual, index) {
-  return anterior + atual;
+var novoArray2 = arr.map(function (item) {
+  return item * 2;
 });
-console.log('sum: ' + sum);
+console.log(novoArray);
 /**
- * filter:
- * filtrar uma array e retornar só os items que atendem a uma condição especifica imposta por você
+ * terceira forma: se o conteúdo do método é só uma linha,
+ * e essa linha for pra retornar algum valor,
+ * tire o abre-chave e fecha-chave e o "return"
+ * 
+ * Observação do Nipodemos: pra te falar a verdade, acho que ficou limpo até demais a sintaxe,
+ * fica até meio dificil perceber de cara que é uma função anonima com parametro item que retorna item * 2 
  */
 
-var filter = arr.filter(function (item) {
-  return item % 2 === 0;
+var novoArray3 = arr.map(function (item) {
+  return item * 2;
 });
-console.log(filter);
+console.log(novoArray);
 /**
- * find:
- * encontrar um item específico na lista que atenda a condição imposta
+ * detalhe:
+ * vc pode retornar normalmente strings, numeros, arrays e talz, mas quando vc for retornar um objeto, tem uma     sintaxe diferente
+ * isso é porque quando vc coloca o abre-chaves ali, ele já acha que é um método que tá seguindo a segunda forma
+ * que eu citei umas linhas acima
  */
 
-var find = arr.find(function (item) {
-  return item === 4;
-});
-console.log(find);
+var novoArray4 = arr.map(function (item) {
+  nome: 'diego';
+}); //! ERRO, NÃO FAÇAM ISSO GENTE
+
+console.log(novoArray);
+/**
+ * modo correto:
+ * coloque parenteses ao redor do objeto
+ */
+
+var novoArray5 = arr.map(function (item) {
+  return {
+    nome: 'diego'
+  };
+}); //AGORA SIM TA CERTO
+
+console.log(novoArray);
--- main.js
+++ main.js
@@ -1,40 +1,47 @@
 const arr = [1,2,3,4,5,7,10,13];
 
 /**
- * map:
- * ele entra em cada um dos items no array e  executa uma function sobre ele.
- * No final é retornado uma nova array com as  * modificações feitas
+ * primeira forma: retire a palavra function e coloque um sinal de igual e maior antes do abre-chaves 
  */
-const novoArray = arr.map(function(item, index){
+const novoArray = arr.map((item)=>{
     return item * 2;
 })
 console.log(novoArray);
 
 
 /**
- * reduce:
- * basicamente é uma função para juntar todos os valores dentro de uma array de números
+ * segunda forma: se o método tem só um parâmetro, vc pode tirar os parenteses antes da seta
  */
-const sum = arr.reduce(function (anterior, atual, index){
-    return anterior + atual;
+const novoArray2 = arr.map( item => {
+    return item * 2;
 })
-console.log('sum: '+sum);
-
-/**
- * filter:
- * filtrar uma array e retornar só os items que atendem a uma condição especifica imposta por você
- */
-const filter = arr.filter(function(item){
-    return item % 2 === 0;
-})
-console.log(filter);
+console.log(novoArray);
 
 
 /**
- * find:
- * encontrar um item específico na lista que atenda a condição imposta
+ * terceira forma: se o conteúdo do método é só uma linha,
+ * e essa linha for pra retornar algum valor,
+ * tire o abre-chave e fecha-chave e o "return"
+ * 
+ * Observação do Nipodemos: pra te falar a verdade, acho que ficou limpo até demais a sintaxe,
+ * fica até meio dificil perceber de cara que é uma função anonima com parametro item que retorna item * 2 
+ */ 
+const novoArray3 = arr.map(item => item * 2);
+console.log(novoArray);
+
+
+/**
+ * detalhe:
+ * vc pode retornar normalmente strings, numeros, arrays e talz, mas quando vc for retornar um objeto, tem uma     sintaxe diferente
+ * isso é porque quando vc coloca o abre-chaves ali, ele já acha que é um método que tá seguindo a segunda forma
+ * que eu citei umas linhas acima
  */
-const find = arr.find(function(item){
-    return item === 4;
-})
-console.log(find);
+const novoArray4 = arr.map(item => {nome: 'diego'}); //! ERRO, NÃO FAÇAM ISSO GENTE
+console.log(novoArray);
+/**
+ * modo correto:
+ * coloque parenteses ao redor do objeto
+ */
+const novoArray5 = arr.map(item => ({nome: 'diego'}) ); //AGORA SIM TA CERTO
+console.log(novoArray);
+
