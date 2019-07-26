b703fa199f5ecba9b8a5e3e44b9601f5c2aeede2
nipodemos <nipodemos13@gmail.com>
2019-07-26 02:52:16 -0300

    Configurando webpack
    nossa, muitas coisas numa aula só, vamos ver se dá pra explicar tudo
    
    primeiro fui no package.json e alterei
    as dependencies para serem devDependencies
    que significa que são dependencias que só serão usadas
    na hora de desenvolver, não são usadas em produção
    
    instalei o webpack e o webpack-cli
    
    depois tive que criar o arquivo webpack.config.js
    pra configurar ele
    
    depois tive que instalar o pactoe babel-loader
    que é uma das coisas que vamos usar junto do webpack
    
    mudei o script no package.json
    para o comando yarn dev
    executar na verdade o webpack, e aí o
    webpack executa o babel da forma dele. sei lá pra que isso
    
    pra testar se tava tudo funcionando,
    criei o arquivo funcoes.js só pra ver se eu consegueria
    pegar a funcao de um arquivo e usar em um outro arquivo
    e funciona :D

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
-const nome = 'Diego';
-const idade = 23;
-
-/**
- * simples porém muito útil
- * na hora que vc tiver definindo objetos, caso a
- * key seja igual o value, vc pode omitir um dos dois
- */
-
-//do jeito antigo e repetitivo fica assim:
-const usuario = {
-    nome: nome,
-    idade: idade,
-    empresa: 'Rocketseat',
-};
-
-
-// do jeito bom fica assim, sem repetição
-const usuario2 = {
-    nome,
-    idade,
-    empresa: 'Rocketseat',
-};
+export function soma(a,b) {
+    return a + b;
+}
