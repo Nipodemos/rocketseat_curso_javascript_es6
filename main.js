e00df6abed468cdaf38be0c10954923ab5d5f240
Nipodemos <nipodemos13@gmail.com>
2019-07-26 01:14:41 -0300

    operadores rest && spread
    tive que executar o comando:
    yarn add @babel/plugin-proposal-object-rest-spread
    
    depois tive que editar o .babelrc, mais detalhes nesse commit
    
    REST
    serve para quando vc não sabe
    a quantidade de variaveis num objeto
    mas quer pegar todas
    
    também serve pra quando sua função pode receber
    vários parametros que vc não sabe quanto
    
    SPREAD
    funciona igualzinho no dart
    vc coloca ... antes e aí ele pega todos os itens daquela array
    e coloca ele dentro dessa outra array que vc ta
    querendo montar
    
    mesma coisa com objetos, em vez de pegar a referencia
     a aquele objeto, ele pega tudo que ta
    dentro dele e coloca nesse novo objeto que ce ta
    fazendo

--- main.js
+++ main.js
@@ -1,46 +1,95 @@
-const Usuario = {
+// rest
+const usuario = {
     nome: 'Diego',
-    idade:23,
-    endereco: {
-        cidade: 'RS',
-        estado: 'SC',
-    },
-}
-
-/**
- * tendo como exemplo o objeto acima, se eu fosse pegar várias
- * informações dele, as coisas podem ficar bem verbosas:
- */
-const nome = usuario.nome;
-const idade = usuario.idade;
-const cidade = usuario.endereco.estado;
+    idade: 23,
+    empresa: 'Rocketseat'
+};
 
 
 /**
- * aí que entra a desestruturação:
- * de certa forma é como declarar várias variaveis numa linha só
- * a diferença é que ela pega o valor de um objeto, deixando mais limpo e intuitivo o código
+ * rest em variaveis é útil pra em vez de vc pegar algo
+ * especifico de um objeto, vc pode pegar uma parte
+ * numa variável, e pegar o resto do objeto na outra 
+ * variavel
  */
-const { nome, idade, endereco: { cidade } } = usuario;
 
-console.log(nome);
-console.log(idade);
-console.log(cidade);
+ 
+const {nome, ...resto} = usuario;
+//nome = Diego
+//rest = {idade: 23, empresa: 'Rocketseat'}
+console.log(nome, resto);
 
 
 /**
- * voce pode também usar desestruturação nos parametros da função
- * útil pra vc pegar já valores separados do objeto
- * talvez fique até mais facil de entender na hora de ler o código
- * ou não
+ * dá pra usar em arrays também, só preste atenção na
+ * sintaxe. Aqui vc usa colchetes pra pegar os valores
  */
+const arra = [1,2,3,4,5];
 
- // versão sem desestruturação
-function mostraNome(usuario) {
-    console.log(usuario.nome);
-}
+const [a, b, ...c] = arra;
+// a = 1
+// b = 2
+// c = [ 3, 4, 5 ]
+console.log(a);
+console.log(b);
+console.log(c);
 
-// com a desestruturação
-function mostraNome2( {nome, idade} ) {
-    console.log(nome,idade);
+
+/**
+ * rest é bacana pra casos em que vc não sabe quantos
+ * parametros a função vai receber
+ * vc pode definir um primeiro ou segundo padrão, e 
+ * usar o rest pra pegar o resto deles 
+*/
+
+// vamos super que vc queria receber quantos números 
+// quiser. Se colocar assim fica muito ruim.
+function soma(a,b,c ) {
+    return a + b + c;
 }
+console.log(soma(1,3));
+
+//mas se usar o operador rest:
+// params = [ 1, 3, 5, 6, 1]
+function soma2(...params){
+    return params.reduce((anterior, atual) => {
+        return anterior + atual;
+    });
+}
+console.log(soma2(1,3,5,6,1));
+
+// ou vc pode pegar alguns valores e colocar em 
+// variaveis especificas e receber o resto em uma
+// outra variavel
+function soma3(a,b, ...params) {
+    return params;
+}
+console.log(soma3(1,3,5,7,3,1,9,5,7));
+
+
+// SPREAD
+/**
+ * se usa igualzinho usa no dart
+ * to com preguiça de digitar, lê aí
+ */
+const arr1 = [1,2,3];
+const arr2 = [4,5,6];
+
+/**
+ * pegando todos os itens da array e colocando nessa
+ * outra array, em vez de colocar só a referencia
+ * aquela array, que é o que aconteceria sem o spread
+ */
+const arr3 = [...arr1, ...arr2];
+
+const usuario1 = {
+    nome: 'Diego',
+    idade: 23,
+    empresa: 'Rocketseat'
+};
+
+const usuario2 = {
+    ...usuario1,
+    nome: 'Gabriel'
+};
+console.log(usuario2);
