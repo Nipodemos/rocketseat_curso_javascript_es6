3bd7957dc18dae3f9094fa58eb0780c3533b245b
nipodemos <nipodemos13@gmail.com>
2019-07-26 11:47:30 -0300

    2º exercicio
    o exercicio 2.2 eu errei, tudo bem
    
    mas o exercicio 2.4 eu simplesmente não soube fazer...
    e esqueci completamente do operador spread que é perfeito nesse caso
    bom, mas eu aprendi um pouco mais :D

--- main.js
+++ main.js
@@ -1,34 +1,50 @@
-// 1º exercício
-// Para testar seus conhecimentos com classes, crie uma classe com nome "Admin", essa classe deve
-// extender uma segunda classe chamada "Usuario".
-// A classe usuário deve receber dois parâmetros no método construtor, e-mail e senha, e anotá-los
-// em propriedades da classe. A classe "Admin" por sua vez não recebe parâmetros mas deve
-// repassar os parâmetros de e-mail e senha à classe pai e marcar uma propriedade "admin" como
-// true na classe.
-// Agora com suas classes formatadas, adicione um método na classe Usuario chamado isAdmin que
-// retorna se o usuário é administrador ou não baseado na propriedade admin ser true ou não.
+// 2º exercício
+// A partir do seguinte vetor e utilizando os métodos de array (map, reduce, filter e find):
+const usuarios = [
+ { nome: 'Diego', idade: 23, empresa: 'Rocketseat' },
+ { nome: 'Gabriel', idade: 15, empresa: 'Rocketseat' },
+ { nome: 'Lucas', idade: 30, empresa: 'Facebook' },
+];
 
-class Usuario {
-    constructor(email, senha) {
-        this.email = email;
-        this.senha = senha;
-    }
-    
-    
-    isAdmin(){
-        return this.admin === true;
-    }
-}
+// 2.1 Utilizando o map
+// Crie uma variável que contenha todas idades dos usuários: [23, 15, 30]
+const idades = usuarios.map((item)=>{
+    return item.idade;
+})
+console.log(idades);
 
-class Admin extends Usuario {
-    constructor(email, senha) {
-        super(email,senha);
-        
-        this.admin = true;
-    }
-}
 
-const User1 = new Usuario('email@teste.com', 'senha123');
-const Adm1 = new Admin('email@teste.com', 'senha123');
-console.log(User1.isAdmin()) // false
-console.log(Adm1.isAdmin()) // true
+// 2.2 Utilizando o filter
+// Crie uma variáveis que tenha apenas os usuários que trabalham na Rocketseat e com mais de 18
+// anos: [{ nome: 'Diego', idade: 23, empresa: 'Rocketseat' }]
+const trabalhadores = usuarios.filter((user)=>{
+    return (user.empresa === 'Rocketseat' && user.idade >= 18) ;
+})
+console.log(trabalhadores);
+
+
+// 2.3 Utilizando o find
+// Crie uma variável que procura por um usuário que trabalhe na empresa Google: undefined
+const googleWorkers = usuarios.find((usuario)=>{
+    return usuario.empresa === 'Google';
+})
+console.log(googleWorkers);
+
+
+// 2.4 Unindo operações
+// Multiplique a idade de todos usuários por dois e depois realize um filtro nos usuários que possuem
+// no máximo 50 anos:
+// Resultado:
+// [
+//  { nome: 'Diego', idade: 46, empresa: 'Rocketseat' },
+//  { nome: 'Gabriel', idade: 30, empresa: 'Rocketseat' },
+// ]
+const treta = usuarios.map((user)=>{
+    return ({...user, idade: user.idade *2})
+}).filter((user)=>{
+    return user.idade <= 50;
+})
+
+console.log(treta);
+
+
